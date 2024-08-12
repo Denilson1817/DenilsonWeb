c1fd19595a015afba377ea32e96ad7e1be6ec439
denilson <denilson_peco@hotmail.com>
2024-08-12 12:34:10 -0600

    update skills

--- js/script.js
+++ js/script.js
@@ -21,14 +21,18 @@ function barSkills(){
         let skill = document.getElementsByClassName("progress");
         skill[0].classList.add("javascript");
         skill[1].classList.add("htmlcss");
-        skill[2].classList.add("python");
-        skill[3].classList.add("net");
+        skill[2].classList.add("cplus");
+        skill[3].classList.add("python");
         skill[4].classList.add("php");
-        skill[5].classList.add("communication");
-        skill[6].classList.add("teamWork");
-        skill[7].classList.add("creativity");
-        skill[8].classList.add("dedication");
-        skill[9].classList.add("adminProjects");
+        skill[5].classList.add("reactjs");
+        skill[6].classList.add("angular");
+        skill[7].classList.add("communication");
+        skill[8].classList.add("english");
+        skill[9].classList.add("teamWork");
+        skill[10].classList.add("creativity");
+        skill[11].classList.add("dedication");
+        skill[12].classList.add("adminProjects");
+        skill[13].classList.add("methodologies");
     }
 }
 window.onscroll = function(){
